7728a2179a5edc7331fba2bd624c23b8f3a3ad78
Ali <zhidli@a483e7872832.ant.amazon.com>
2020-03-10 17:35:43 -0700

    First draft of shotty script - list instances

---
+++ shotty/shotty.py
@@ -0,0 +1,9 @@
+# command to run the file: pipenv run python shotty/shotty.py
+import boto3
+
+if __name__== '__main__':
+    session=boto3.Session(profile_name='shotty')
+    ec2=session.resource('ec2')
+
+    for i in ec2.instances.all():
+        print(i)
